affd99ac43d51c74b20a4c2b349e48f17cf4e0cd
Hamdi Rizal <hamdirizal@gmail.com>
2023-07-19 20:12:09 +0700

    Update

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,59 @@
 import inquirer from 'inquirer';
-import { getAppState } from './utils.js';
+import { readAppState, writeAppState } from './utils.js';
+import { AppState } from './types.js';
+import fs, { existsSync } from 'fs';
 
-getAppState();
+const processTheGivenSavePath = (savePath) => {
+  if(existsSync(savePath)) {
+    const appState:AppState = readAppState();
+    writeAppState({
+      ...appState,
+      gameSavePath: savePath
+    });
+    console.log('Directory exists')
+  }
+  else {
+    console.log('Directory does not exist');
+  }
+  
+
+}
+
+const noSavePath = () => {
+  inquirer
+    .prompt({
+      type: 'input',
+      name: 'save_path',
+      message: 'Please enter the path where the save file exists',
+    })
+    .then((answers) => {
+      processTheGivenSavePath(answers.save_path);
+    });
+}
+
+const noSaveFile = () => {
+  inquirer
+    .prompt({
+      type: 'input',
+      name: 'save_path',
+      message: 'Please enter the path where the save file exists',
+    })
+    .then(() => {
+      console.log('The wolf mauls you. You die. The end.');
+    });
+}
+
+const success = () => {}
+
+const main = () => {  
+  const appState:AppState = readAppState();
+  if(appState.gameSavePath) {
+    console.log("Save path:", appState.gameSavePath);
+  }
+  else{
+    noSavePath();
+  }
+  
+}
+
+main();
--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import { AppState } from './types.js';
 import { APP_STATE_FILENAME } from './constants.js';
 
-export const getAppState = (): AppState => {
+export const readAppState = (): AppState => {
   const defaultValue: AppState = {
     gameSavePath: ''
   }
@@ -13,6 +13,14 @@ export const getAppState = (): AppState => {
   }
 
   //If file exists, read it and return the object
+  const fileContent = fs.readFileSync(`./${APP_STATE_FILENAME}`, 'utf-8');
+  if(fileContent) {
+    return JSON.parse(fileContent);
+  }
 
   return defaultValue;
+};
+
+export const writeAppState = (appState: AppState) => {
+  fs.writeFileSync(`./${APP_STATE_FILENAME}`, JSON.stringify(appState));
 };
