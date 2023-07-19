7617d747f9adf8ae052b382969d1127e30dbd363
Hamdi Rizal <hamdirizal@gmail.com>
2023-07-19 09:43:38 +0700

    First commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import readline from 'readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Please enter your name: ', (name) => {
+  console.log(`Hello, ${name}!`);
+  rl.close();
+});
