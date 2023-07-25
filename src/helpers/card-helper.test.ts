e7e73ba7dbad629e9f1f634faa1e9e2568c25ca1
Hamdi Rizal <hamdirizal@gmail.com>
2023-07-25 14:56:58 +0700

    Updated test

--- src/helpers/card-helper.test.ts
+++ src/helpers/card-helper.test.ts
@@ -1,5 +1,17 @@
-describe('sum module', () => {
-  test('Extract card id from name', () => {
-    expect(3).toBe(3);
+import { extractIdFromCardName } from "./card-helper.js";
+
+describe('Func extractIdFromCardName', () => {
+  test('Given valid parameter, it should return correct id', () => {
+    expect(extractIdFromCardName('[0] Hello World')).toBe(0);
+    expect(extractIdFromCardName('[99] Foo bar')).toBe(0);
+  });
+  test('Given invalid parameter, it should return -1', () => {
+    expect(extractIdFromCardName('[0 Hello World')).toBe(-1);
+    expect(extractIdFromCardName('99] Foo bar')).toBe(-1);
+    expect(extractIdFromCardName('asdf')).toBe(-1);
+    expect(extractIdFromCardName('[] Foo')).toBe(-1);
+    expect(extractIdFromCardName('[]')).toBe(-1);
+    expect(extractIdFromCardName('')).toBe(-1);
+    expect(extractIdFromCardName('99')).toBe(-1);
   });
 });
