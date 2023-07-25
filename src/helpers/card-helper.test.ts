8ba7dbf3653f851ed4f6e017b5e9b17538a3795a
Hamdi Rizal <hamdirizal@gmail.com>
2023-07-25 15:01:49 +0700

    Setup babel for test, and updated the test itself

--- src/helpers/card-helper.test.ts
+++ src/helpers/card-helper.test.ts
@@ -3,7 +3,7 @@ import { extractIdFromCardName } from "./card-helper.js";
 describe('Func extractIdFromCardName', () => {
   test('Given valid parameter, it should return correct id', () => {
     expect(extractIdFromCardName('[0] Hello World')).toBe(0);
-    expect(extractIdFromCardName('[99] Foo bar')).toBe(0);
+    expect(extractIdFromCardName('[99] Foo bar')).toBe(99);
   });
   test('Given invalid parameter, it should return -1', () => {
     expect(extractIdFromCardName('[0 Hello World')).toBe(-1);
